d06aad440df9275b3b4fb04a6b94911fd8a27bf5
Shalini8 <shalu8mar@gmail.com>
2021-05-27 20:32:21 +0530

    Calculate wage of employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -12,3 +12,27 @@ if (employeeChecker == IS_ABSENT) {
 } else {
     console.log("Employee is Absent!");
 }
+
+//Generating a random number
+let empSalary = Math.floor((Math.random() * 10) % 3);
+
+//Assigning varaibles value as const as our intention is never changing there values
+const PART_TIME_HOURS = 4;
+
+const FULL_TIME_HOURS = 8;
+
+const WAGE_PER_HOUR = 20;
+
+//Switch case to calculate wage according to random values generated
+switch(empSalary) {
+    case 0:
+        console.log("Employee wage is 0");
+        break;
+
+    case 1:
+        console.log("Employee wage is $" + (PART_TIME_HOURS * WAGE_PER_HOUR));
+        break;
+
+    case 2:
+        console.log("Employee wage is $" + (FULL_TIME_HOURS * WAGE_PER_HOUR));
+}
