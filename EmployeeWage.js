7d8d1ad6ed233c8fb3ead7448e4b2978330e853f
Shalini8 <shalu8mar@gmail.com>
2021-05-28 07:29:43 +0530

    [Modified] use function getworkingHrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,12 +1,11 @@
 //Generating a random number
 let employeeChecker =  Math.floor((Math.random() * 10) % 2);
 
-//Assigning varaibles value as const as our intention is never changing there values
 const IS_ABSENT = 0;
 
 const IS_PRESENT = 1;
 
-//If-Else statement to check for presence and absence of employee
+//If-Else statement to check for employee is absent or present..
 if (employeeChecker == IS_ABSENT) {
     console.log("Employee is present!");
 } else {
@@ -14,25 +13,32 @@ if (employeeChecker == IS_ABSENT) {
 }
 
 //Generating a random number
-let empSalary = Math.floor((Math.random() * 10) % 3);
+let empCheckHrs = Math.floor((Math.random() * 10) % 3);
 
-//Assigning varaibles value as const as our intention is never changing there values
+//Assigning variables value as const as our intention is never changing there values
+const IS_PART_TIME =1;
+const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
-
 const FULL_TIME_HOURS = 8;
-
 const WAGE_PER_HOUR = 20;
 
-//Switch case to calculate wage according to random values generated
-switch(empSalary) {
-    case 0:
-        console.log("Employee wage is 0");
-        break;
+let empHours = 0;
+ //Function of switch case to calculate wage according to random values generated
 
-    case 1:
-        console.log("Employee wage is $" + (PART_TIME_HOURS * WAGE_PER_HOUR));
-        break;
+function getWorkingHours(empCheckHrs){
+    switch(empCheckHrs) {
+        case 1:
+            return PART_TIME_HOURS;
+    
+        case 2:
+            return FULL_TIME_HOURS;
 
-    case 2:
-        console.log("Employee wage is $" + (FULL_TIME_HOURS * WAGE_PER_HOUR));
+        default:
+            return 0;
+
+    }
 }
+
+empHours = getWorkingHours(empCheckHrs);
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("working hrs is: " + empHours ,"Employee wage is $" + empWage);
